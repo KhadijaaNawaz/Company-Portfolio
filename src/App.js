dea58841a7ae8be2d422f053a02e25cdf186fd9b
KhadijaaNawaz <khadijanawaz8302@gmail.com>
2024-12-10 19:32:49 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
+import './App.css';
+
 
 const App = () => {
   return (
