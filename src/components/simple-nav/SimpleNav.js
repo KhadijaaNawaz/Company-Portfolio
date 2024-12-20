c16f2f01e6778ca3d4fb2755daa4494abf9694da
KhadijaaNawaz <khadijanawaz8302@gmail.com>
2024-12-20 14:30:10 +0500

    changes

--- src/components/simple-nav/SimpleNav.js
+++ src/components/simple-nav/SimpleNav.js
@@ -3,22 +3,40 @@ import styles from "./simplenav.module.css";
 
 const SimpleNav = () => {
   const navItems = [
-    "ERP",
-    "Accounting Software",
-    "Global Business Management",
-    "CRM",
-    "HR",
-    "Field Service",
-    "PSA",
-    "Connectors",
+    { name: "ERP", link: "#erpSectionWrapper" },
+    { name: "CRM", link: "#crmSectionWrapper" },
+    { name: "Accounting Software", link: "#accountingSection" },
+    { name: "Global Business Management", link: "#globalSection" },
+    { name: "HR", link: "#hrSection" },
+    { name: "Field Service", link: "#fieldServiceSection" },
+    { name: "PSA", link: "#psaSection" },
+    { name: "Connectors", link: "#connectorsSection" },
   ];
 
+  const smoothScrollTo = (link) => {
+    const targetElement = document.querySelector(link);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
+    <nav className={styles.simple_navbar}>
+      <ul className={styles.simple_navList}>
         {navItems.map((item, index) => (
-          <li key={index} className={styles.navItem}>
-            {item}
+          <li key={index} className={styles.simple_navItem}>
+            <a
+              href={item.link}
+              onClick={(e) => {
+                e.preventDefault();
+                smoothScrollTo(item.link);
+              }}
+            >
+              {item.name}
+            </a>
           </li>
         ))}
       </ul>
